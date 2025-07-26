d691bd08bc5492006980aa08233e716cc3747d8d
halfcomplete <ehu3001@gmail.com>
2025-07-26 12:26:10 +1000

    Revert "Revert "UI changes""

---
+++ AshborneGame/_Core/Game/DescriptionHandling/LookDescription.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AshborneGame._Core.Game.DescriptionHandling
+{
+    public class LookDescription
+    {
+        public string FirstLook { get; set; }
+        public string SecondLook { get; set; }
+        public string RepeatLook { get; set; }
+
+        public int LookCount { get; set; } = 0;
+
+        public LookDescription(string firstLook, string? secondLook = null, string? repeatLook = null)
+        {
+            FirstLook = firstLook;
+            SecondLook = secondLook ?? "You look around once more. Nothing has changed.";
+            RepeatLook = repeatLook ?? "You look around again, but everything remains the same.";
+        }
+
+        public LookDescription()
+        {
+            FirstLook = "You look around, taking in your surroundings.";
+            SecondLook = "You look around again, but nothing has changed.";
+            RepeatLook = "You glance around once more, but everything remains the same.";
+        }
+    }
+}
--- AshborneGame/_Core/SceneManagement/LocationFactory.cs
+++ AshborneGame/_Core/SceneManagement/LocationFactory.cs
@@ -1,10 +1,11 @@
 ﻿using AshborneGame._Core.Game.DescriptionHandling;
+using AshborneGame._Core.Globals.Constants;
 
 namespace AshborneGame._Core.SceneManagement
 {
     public static class LocationFactory
     {
-        public static Location CreateLocation(Location location, string lookDescription, FadingDescription fading, SensoryDescription sensory, AmbientDescription? ambient = null, ConditionalDescription? conditional = null)
+        public static Location CreateLocation(Location location, LookDescription lookDescription, FadingDescription fading, SensoryDescription sensory, AmbientDescription? ambient = null, ConditionalDescription? conditional = null)
         {
             var descriptionComposer = new DescriptionComposer(
                 lookDescription,
@@ -17,6 +18,34 @@ namespace AshborneGame._Core.SceneManagement
             location.SetDescriptionComposer(descriptionComposer);
 
             return location;
+        }
+
+        /// <summary>
+        /// Adds mutual exits between two locations in both directions.
+        /// </summary>
+        /// <param name="location1">The first location.</param>
+        /// <param name="location2">The second location.</param>
+        /// <param name="direction">The direction from the first location to the second location.</param>
+        /// <exception cref="ArgumentNullException">Thrown when any of the locations provided are null.</exception>
+        public static void AddMutualExits(Location location1, Location location2, string direction)
+        {
+            if (location1 == null || location2 == null)
+                throw new ArgumentNullException("LocationFactory.cs: Locations provided for 'AddMutualExits.cs' cannot be null.");
+
+            if (!location1.Exits.ContainsKey(direction) && !location2.Exits.ContainsKey(direction))
+            {
+                location1.Exits.Add(direction, location2);
+                location2.Exits.Add(DirectionConstants.CardinalDirectionOppositesMap[direction], location1);
+            }
+            else
+            {
+                throw new InvalidOperationException($"LocationFactory.cs: Cannot add mutual exits to locations '{location1.Name.ReferenceName}' and '{location2.Name.ReferenceName}'. One or both locations already have an exit in that direction.");
+            }
+        }
+
+        public static Scene CreateScene(string sceneName, string sceneID, List<Location>? locations = null)
+        {
+            return new Scene(sceneID, sceneName, locations);
         }
     }
 }
