8d4e0c8a35936491c0b66aea9cdc201dff3163d2
ehu29 <ehu3001@gmail.com>
2026-05-07 18:52:59 +1000

    feat: remove EmotionCollection

--- AshborneGame/_Core/EmotionSystem/EmotionProfile.cs
+++ AshborneGame/_Core/EmotionSystem/EmotionProfile.cs
@@ -6,6 +6,10 @@ namespace AshborneGame._Core.EmotionSystem
 {
     public class EmotionProfile
     {
+        /// <summary>
+        /// A list of all the active emotion modifiers currently affecting the character. Each modifier can contribute positively or negatively to the intensity of a specific emotion type, and their effects are evaluated lazily based on the time elapsed since they were applied. This allows for dynamic changes in the character's emotional state over time, as modifiers decay or are removed when depleted.
+        /// If there are no modifiers active for a particular emotion type, the intensity of that emotion is considered to be 0. The overall intensity of each emotion type is calculated by summing the current values of all relevant modifiers and clamping the result between -100 and 100 to maintain a reasonable range for emotional intensity.
+        /// </summary>
         private List<EmotionModifier> _modifiers = new List<EmotionModifier>();
 
         public void AddModifier(EmotionModifier modifier)
