a931c16e854e517d058368f44b0b2f3ea362f41a
ehu29 <ehu3001@gmail.com>
2026-07-08 21:30:53 +1000

    feat: support saving/loading

--- AshborneGame/_Core/Data/BOCS/ItemSystem/ItemBehaviours/Combat/OnEnemyUseDealDamageBehaviour.cs
+++ AshborneGame/_Core/Data/BOCS/ItemSystem/ItemBehaviours/Combat/OnEnemyUseDealDamageBehaviour.cs
@@ -3,16 +3,17 @@ using AshborneGame._Core.Data.BOCS.ItemSystem.ItemCapabilities;
 using AshborneGame._Core.Data.BOCS.ItemSystem.ItemBehaviours;
 using AshborneGame._Core.Game;
 using AshborneGame._Core.Globals.Services;
+using AshborneGame._Core.SaveSystem.Data.BOCSDTOs;
+using AshborneGame._Core.SaveSystem.Serialisation;
+using System.Text.Json;
 
 namespace AshborneGame._Core.Data.BOCS.ItemSystem.ItemBehaviours.Combat
 {
     public class OnEnemyUseDealDamageBehaviour : Behaviour, IUsable, ICanDamage
     {
-        public int BaseDamage { get; set; }
         public double BaseDamage { get; set; }
         public bool ConsumeOnUse { get; private set; }
 
-        public OnEnemyUseDealDamageBehaviour(int baseDamage, bool consumeOnUse = false)
         public OnEnemyUseDealDamageBehaviour(double baseDamage, bool consumeOnUse = false)
         {
             BaseDamage = baseDamage;
@@ -29,6 +30,22 @@ namespace AshborneGame._Core.Data.BOCS.ItemSystem.ItemBehaviours.Combat
         public override OnEnemyUseDealDamageBehaviour DeepClone()
         {
             return new OnEnemyUseDealDamageBehaviour(BaseDamage, ConsumeOnUse);
+        }
+
+
+        private record SaveData(double BaseDamage, bool ConsumeOnUse);
+
+        public override BehaviourSaveData? GetSaveState(SaveLoadContext context)
+        {
+            return new BehaviourSaveData(SaveId, JsonSerializer.SerializeToElement((new SaveData(BaseDamage, ConsumeOnUse))));
+        }
+
+        public override void LoadSaveState(BehaviourSaveData data, SaveLoadContext context)
+        {
+            SaveData save = JsonSerializer.Deserialize<SaveData>(data.State) ?? throw new InvalidDataException("Failed to deserialise ApplyStatusEffectOnUseBehaviour save data.");
+
+            BaseDamage = save.BaseDamage;
+            ConsumeOnUse = save.ConsumeOnUse;
         }
     }
 }
