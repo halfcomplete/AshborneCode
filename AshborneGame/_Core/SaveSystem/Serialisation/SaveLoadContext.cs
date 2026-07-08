8c7c110b8892aaaced1d31351a7cb8a99372128c
ehu29 <ehu3001@gmail.com>
2026-07-08 16:38:25 +1000

    feat: create SaveLoadContext

---
+++ AshborneGame/_Core/SaveSystem/Serialisation/SaveLoadContext.cs
@@ -0,0 +1,48 @@
+﻿using AshborneGame._Core.Data.BOCS;
+using AshborneGame._Core.Data.Definitions;
+using AshborneGame._Core.Data.IDSystem;
+using AshborneGame._Core.LocationManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AshborneGame._Core.SaveSystem.Serialisation
+{
+    public sealed class SaveLoadContext
+    {
+        public IInstanceRegistry InstanceRegistry { get; }
+        public ILocationRegistry LocationRegistry { get; }
+
+        // Save: object -> ID
+        public InstanceID ToId(BOCSObject obj) => obj.InstanceID;
+        public DefinitionID ToId(Location loc) => loc.DefinitionID;
+
+        // Load phase 2 only: ID -> object
+        public BOCSObject ResolveObject(InstanceID id)
+        {
+            if (!InstanceRegistry.TryGet(id, out var obj))
+            {
+                throw new InvalidOperationException($"[SaveLoadContext]: Failed to resolve object with InstanceID {id}.");
+            }
+
+            return obj;
+        }
+
+        public Location ResolveLocation(DefinitionID id)
+        {
+            if (!LocationRegistry.TryGetLocationByDefinitionID(id, out var loc))
+            {
+                throw new InvalidOperationException($"[SaveLoadContext]: Failed to resolve location with DefinitionID {id}.");
+            }
+
+            return loc;
+        }
+
+        public T? TryResolveObject<T>(InstanceID? id) where T : class
+        {
+
+        }
+    }
+}
